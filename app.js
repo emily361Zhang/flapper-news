55b20e8d2ee7fd80a74c7ef390759a4bd5abebc5
emily361Zhang <ezhang060@gmail.com>
2017-01-28 15:07:18 -0500

    Routing Views with Angular

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router'])
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+// factory
+app.factory('posts', [function() {
+  //service body
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
 
 app.controller('MainCtrl', [
   '$scope',
-  function ($scope) {
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
+  'posts',
+  function ($scope, posts) {
+    $scope.posts = posts.posts;
 
     $scope.addPost = function() {
-      if (!$scope.title || $scope.title === '') {
-        return;
-      }
+      if (!$scope.title || $scope.title === '') { return; }
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
@@ -28,6 +45,4 @@ app.controller('MainCtrl', [
       post.upvotes += 1;
     };
 
-  }
-
-]);
+  }]);
